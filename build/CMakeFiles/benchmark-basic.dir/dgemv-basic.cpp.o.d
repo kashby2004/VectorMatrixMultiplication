f7cf2fa34a7545ba43016930e9ece71293c47bed
Kaitlyn Ashby <akay47@login18.chn.perlmutter.nersc.gov>
2025-11-20 15:41:16 -0800

    logs

---
+++ build/CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-basic.dir/dgemv-basic.cpp.o: \
+ /global/homes/a/akay47/VectorMatrixMultiplication/dgemv-basic.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/benchmark-vectorized.dir/dgemv-vectorized.cpp.o: \
+ /global/homes/a/akay47/VectorMatrixMultiplication/dgemv-vectorized.cpp \
+ /usr/include/stdc-predef.h
